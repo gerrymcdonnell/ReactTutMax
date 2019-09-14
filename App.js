bb8de324bf121a210939dd0a8ef6efa5ebf81bb6
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-09-14 19:56:35 +0100

    get the users location

--- App.js
+++ App.js
@@ -9,13 +9,17 @@ import Footer from './src/components/Footer';*/
 import React, { Component } from 'react';
 import { Text, View,Stylesheet } from 'react-native';
 
-//import customcomponet nb convention is capital letter
+//import customcomponet nb. convention is capital letter
 import FetchLocation from './src/components/FetchLocation';
+
 
 export default class HelloWorldApp extends Component {
 
 getUserLocationHandler=()=>{
-
+  console.log('onPress');
+  navigator.geolocation.getCurrentPosition(position=>{
+    console.log(position);
+  },err=>console.log(err));
 }
 
   render() {
